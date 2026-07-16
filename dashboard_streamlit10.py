00ad3683e3f552c2b98e414f0be569365e6a66a3
geethanjaliiiii <geethanjali.tk@spatialty.ai>
2026-07-16 15:28:20 +0530

    Update dashboard_streamlit10.py

--- dashboard_streamlit10.py
+++ dashboard_streamlit10.py
@@ -358,7 +358,7 @@ else:
     DAILY_FORECAST_FILL = "rgba(242,142,43,0.25)"
     TWO_HOUR_COLOR = "#2CA02C"        # Green
     ACTUAL_COLOR = "#636EFA"          # Blue
-    DAY_AHEAD_COLOR = "#00838F"
+    DAY_AHEAD_COLOR = "#6D4C41"
 
     # =====================================================
     # FIRST ROW: DAILY FORECAST GHI ONLY
